900d4f1e26923ffa55ff4f72cca4524bee9f966f
LoneCoder <56129844+15xda@users.noreply.github.com>
2025-04-30 20:25:55 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './components/pages/Home';
 import Layout from './components/pages/Layout';
 import ProductDetails from './components/pages/ProductDetails';
