1bda0a137ffacecabbdbfcaecb1be68ce970c1ff
zaza <adovudzoda@gmail.com>
2025-05-01 08:47:55 +0300

    white placehold backgrounds

--- frontend/src/components/ProductImage.jsx
+++ frontend/src/components/ProductImage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const ProductImage = ({ product }) => {
-  const defaultImage = `https://placehold.co/600x600/transparent/23645C?text=${product?.name || "Gold Perfume"}`;
+  const defaultImage = `https://placehold.co/600x600/white/23645C?text=${product?.name || "Gold Perfume"}`;
 
   const getImageSrc = () => {
     const name = product.name.toLowerCase();
