ccb403d2ff6265e3e3a03d784ec132a100a91f58
zaza <adovudzoda@gmail.com>
2025-05-06 12:47:07 +0300

    fixing cookie for refresh at 6

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -53,8 +53,8 @@ const login = async (req, res) => {
         res
             .cookie('refreshToken', refreshToken, {
                 httpOnly: true,
-                // secure: true,
-                // sameSite: 'None',
+                secure: true,
+                sameSite: 'None',
                 path: '/',
                 maxAge: 31 * 24 * 60 * 60 * 1000
             })
@@ -250,8 +250,8 @@ const confirmEmailVerification = async (req, res) => {
 const logout = (req, res) => {
     res.clearCookie('refreshToken', {
         httpOnly: true,
-        // secure: true,
-        // sameSite: 'None',
+        secure: true,
+        sameSite: 'None',
         path: '/'
     });
     res.json({message: 'Выход выполнен успешно'});
